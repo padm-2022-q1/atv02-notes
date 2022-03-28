b739181e0fc9a40a9e4f0ac2e0f2aa4498d28d04
Diogo S. Martins <santana.martins@ufabc.edu.br>
2022-03-28 18:16:03 -0300

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+## Atividade 02 - Notes!
+
+Para as orientações, consule o enunciado (PDF) no Moodle.
+
+Demo do comportamento esperado do aplicativo.
+
+![Demo GIF](img/note_demo.gif)
---
+++ app/src/main/java/br/edu/ufabc/notes/MainActivity.kt
@@ -0,0 +1,14 @@
+package br.edu.ufabc.notes
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+/**
+ * The main activity.
+ */
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
